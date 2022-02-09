d9609be05f57376d1d6c831b773b3123df4db1d6
liberomgm <vadick.n2018@yandex.ru>
2022-02-09 17:38:47 +0300

    Add WebSite URL

---
+++ Assets/WebSiteURL.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebSiteURL : MonoBehaviour
+{
+    public void URL()
+    {
+        Application.OpenURL("https://ru.wikipedia.org/wiki/Игра");
+    }
+}
